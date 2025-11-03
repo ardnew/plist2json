fc0e26b766ce4072e45db4785ebd3ccfb9c30720
ardnew <andrew@ardnew.com>
2025-11-03 15:43:35 -0600

    Bump version to 0.1.6

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="plist2json",
-    version="0.1.5",
+    version="0.1.6",
     author="ardnew",
     author_email="andrew@ardnew.com",
     description="Convert Apple plist files to JSON format",
